60e65ab28c8794199f7d8d480898b9704d9bfbd8
athulrajgeco <athulraj@tutamail.com>
2021-01-26 12:58:26 +0530

    login page

---
+++ src/views/HomePage.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="">
+    <div class="login">
+      <form class="" action="index.html" method="post">
+        <label for="uname">Username</label>
+        <input type="text" name="uname" v-model="username">
+        <br>
+        <label for="pwd">Password</label>
+        <input type="password" name="pwd" v-model="password">
+        <br>
+        <input type="submit" @click.prevent='login' value="Login">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      username:'',
+      password:''
+    }
+  },
+  methods: {
+    login(){
+      console.log(`${this.username} has password ${this.password}`);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+label{
+  margin-right: 12px;
+}
+</style>

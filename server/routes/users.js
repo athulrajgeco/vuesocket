1e6ee9e11d7a34509ced91ce02b4a2d01aaac896
Athul Raj <athulraj@tutamail.com>
2021-02-13 16:57:48 +0530

    login css

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const cors = require('cors');
 
+let classList = [
+  {
+    sub: 'Programming',
+    fac: 'Prof. Athul Raj',
+    start: '1613149859623'
+  },
+  {
+    sub: 'Signals',
+    fac: 'Prof. Haryhar',
+    start: '1613149857623'
+  },
+  {
+    sub: 'PE',
+    fac: 'Dr. Jaison Mathew',
+    start: '1613149957623'
+  }
+]
+
 let router = express.Router()
 router.use(cors())
 router.use(express.urlencoded({extended: false}))
@@ -10,11 +28,16 @@ const classes = ['101', '303', '507']
 
 router.post('/login',(req, res)=>{
   console.log(req.body);
-  let route = 'student'
-  if(req.body.username.search('TR')==0){
-    route = 'faculty'
+  let data = {
+    user: 'student'
   }
-  res.send(route)
+  if(req.body.username.search('TR')==0){
+    data.user = 'faculty'
+  }
+  else{
+    data.class = classList
+  }
+  res.send(data)
 })
 
 module.exports = router
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -19,22 +19,34 @@
     </div>
     <div class="" v-if="user == 'student'">
       <h1>Ongoing classes</h1>
-      <div class="gridcontainer">
-        <div class="classroom">
+      <div class="flexcontainer">
+        <div v-for='c of ongoingClass' class="notice">
           <h3>Subject</h3>
-          <p></p>
+          <p>{{c.sub}}</p>
           <h3>Faculty</h3>
-          <p></p>
+          <p>{{c.fac}}</p>
           <h3>Start Time</h3>
-          <p></p>
-          <button type="button" name="button">Join</button>
+          <p>{{c.start}}</p>
+          <button type="button" name="button" @click='joinClass'>Join</button>
         </div>
       </div>
       <h1>Starting Soon...</h1>
+      <div class="" v-for='c of soonClass'>
+        <h3>Subject</h3>
+        <p></p>
+        <h3>Faculty</h3>
+        <p></p>
+        <h3>Start Time</h3>
+        <p></p>
+      </div>
     </div>
     <!-- <div class="" v-else>
-      <button type="button" name="button"></button>
+      <button type="button" name="button">Schedule a class</button>
+      <button type="button" name="button">Start class</button>
     </div> -->
+    <div class="modal" v-if="modal" @click='modal = false'>
+
+    </div>
   </div>
 </template>
 
@@ -45,7 +57,10 @@ export default {
     return{
       username:'',
       password:'',
-      user: ''
+      user: '',
+      ongoingClass: [],
+      soonClass: [],
+      modal: false
     }
   },
   methods: {
@@ -58,20 +73,34 @@ export default {
       }
       axios.post(address, data)
       .then(data =>{
-        if(data.data == 'faculty'){
+        if(data.data.user == 'faculty'){
           confirm("Maximize your window to get maximum whiteboard width")
           //this.$router.push('/wb')
           this.user = 'faculty'
         }
         else{
-          // this.$router.push('/stwb')
+          let classList = []
           this.user = 'student'
+          classList = data.data.class
+          classList.forEach((item, i) => {
+            if (item.start < Date.now()) {
+              this.ongoingClass.push(item)
+            } else {
+              this.soonClass.push(item)
+            }
+          })
         }
-        console.log(data);
+        console.log(data.data);
       })
       .catch(err =>{
         console.log(err);
       })
+    },
+    startClass(){
+
+    },
+    joinClass(k){
+      this.modal = true
     }
   }
 }
@@ -96,5 +125,53 @@ export default {
   }
   .griditem2{
     grid-column: 6 /9;
+  }
+  .modal{
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0,0,0,0.85);
+    z-index: 1;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+  }
+  .flexcontainer{
+    display: flex;
+  }
+  .notice{
+    position: relative;
+    background-color: lightblue;
+    margin: 15px 20px;
+    padding: 15px;
+    padding-bottom: 30px;
+    min-width: 250px;
+    border-radius: 0 25px 0 25px;
+  }
+  .notice::before {
+    content: "";
+    position: absolute;
+    background-color: transparent;
+    top: 0px;
+    left: -50px;
+    height: 25px;
+    width: 50px;
+    border-top-right-radius: 25px;
+    box-shadow: 25px 0 0 0 lightblue;
+  }
+  .notice::after {
+    content: "";
+    position: absolute;
+    background-color: transparent;
+    bottom: 0px;
+    right: -50px;
+    height: 25px;
+    width: 50px;
+    border-bottom-left-radius: 25px;
+    box-shadow: -25px 0 0 0 lightblue;
+  }
+  .notice > h3{
+    color: sienna;
+    font-weight: 800;
+    font-size: 22px;
   }
 </style>
